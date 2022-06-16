3b27432d989b6255571ee16aa80be0769850fe68
Stephen Reynolds <sgrjr@deliverance.me>
2022-06-16 14:43:55 -0400

    ready

--- src/Models/Webheads.php
+++ src/Models/Webheads.php
@@ -5,4 +5,9 @@ use Sreynoldsjr\ReynoldsDbf\Models\Model;
 class Webheads extends Model {
      public $table = 'webheads';
      public $fillable = ["INDEX","KEY","ATTENTION", "DATE","BILL_1","BILL_2","BILL_3","BILL_4","COMPANY","STREET","CITY","STATE","POSTCODE","VOICEPHONE","OSOURCE","ISCOMPLETE", "ROOM","DEPT","COUNTRY","FAXPHONE","EMAIL","SENDEMCONF","PO_NUMBER","CINOTE","CXNOTE","TRANSNO","DATESTAMP","TIMESTAMP","LASTDATE","LASTTIME","LASTTOUCH","REMOTEADDR","deleted_at"];
+
+     public function items(){
+          return Webdetails::x()->where("REMOTEADDR", "==", $this->REMOTEADDR)->all();
+     }
+
 }
