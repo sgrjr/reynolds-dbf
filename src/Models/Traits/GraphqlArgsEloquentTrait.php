0bb93beb8b4116d4187b03cb147f53ed59af8b85
Stephen Reynolds <sgrjr@deliverance.me>
2022-06-17 16:12:32 -0400

    changes

--- src/Models/Traits/GraphqlArgsEloquentTrait.php
+++ src/Models/Traits/GraphqlArgsEloquentTrait.php
@@ -4,41 +4,41 @@ trait GraphqlArgsEloquentTrait {
 
     public function graphql($args = []){
 
-        if(isset($options["first"])){
-            $first = $options["first"];
+         $query = $this->orderBy('id','DESC');
+
+        if(isset($args["first"])){
+            $first = $args["first"];
         }else{
             $first = 10;
         }
 
-        if(isset($options["page"])){
-            $page = $options["page"];
+        if(isset($args["page"])){
+            $page = $args["page"];
         }else{
             $page = 1;
         }
 
-        $this = $this->orderBy('id','DESC');
-
-            if(isset($args["filter"])){
-                foreach($args["filter"] AS $key=>$v){
-                    if(strpos($v, "_") !== false){
-                        $f = explode("_",$v,2);
-                    } else{
-                        $f[0]="==";
-                        $f[1]=$v;
-                    }
-                    
-                    $val = trim($f[1]);
-                    if($val === ""){$val = null;}
-                    if($val === "TRUE"){$val = true;}
-                    if($val === "true"){$val = true;}
-                    if($val === "FALSE"){$val = false;}
-                    if($val === "false"){$val = false;}
-
-                    $this = $this->where($key,$f[0],$val);
+        if(isset($args["filter"])){
+            foreach($args["filter"] AS $key=>$v){
+                if(strpos($v, "_") !== false){
+                    $f = explode("_",$v,2);
+                } else{
+                    $f[0]="==";
+                    $f[1]=$v;
                 }
+                
+                $val = trim($f[1]);
+                if($val === ""){$val = null;}
+                if($val === "TRUE"){$val = true;}
+                if($val === "true"){$val = true;}
+                if($val === "FALSE"){$val = false;}
+                if($val === "false"){$val = false;}
 
+                $query->where($key,$f[0],$val);
             }
 
-        return $this;
+        }
+        if($first === 1 && $page === 1){return $query->first();}
+        return $query;
     }
 }
