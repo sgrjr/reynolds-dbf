1e769a60745c85ef4be1fcc3f3199c2e8ed71ff0
Stephen Reynolds <sgrjr@deliverance.me>
2022-07-23 14:58:03 -0400

    fresh install created

--- src/Console/Commands/Install.php
+++ src/Console/Commands/Install.php
@@ -11,7 +11,7 @@ class Install extends Command
      *
      * @var string
      */
-    protected $signature = 'rdbf:install {--seed} {--force} {--teams} {--cache}';
+    protected $signature = 'rdbf:install {--seed} {--force} {--teams} {--cache} {--fresh}';
 
     /**
      * The console command description.
@@ -27,8 +27,11 @@ class Install extends Command
      */
     public function handle()
     {
+
+        if($this->option('force')) return $this->freshInstall();
+
         $this->comment(PHP_EOL."Creating All Database Tables from DBFS...".PHP_EOL);
-        
+            
         // Build the tables
         ReynoldsDbf::install($this->option('force'));
         
@@ -45,5 +48,24 @@ class Install extends Command
         } 
 
         $this->comment(PHP_EOL."Creating all tables complete.".PHP_EOL);
+    }
+
+    public function freshInstall(){
+
+        $this->comment(PHP_EOL."Beginning Fresh install of aplication data.".PHP_EOL);
+
+        $this->comment(PHP_EOL."-->1. migrate all tables.".PHP_EOL);
+        ReynoldsDbf::install(true);
+
+        $this->comment(PHP_EOL."-->2. seed those tables.".PHP_EOL);
+        ReynoldsDbf::seed(true);
+
+        $this->comment(PHP_EOL."-->3. seed teams.".PHP_EOL);
+        ReynoldsDbf::seed(true);
+
+        $this->comment(PHP_EOL."-->4. cache everything.".PHP_EOL);
+        Artisan::call('rdbf:cache');
+
+        $this->comment(PHP_EOL."Fresh install of aplication data completed :).".PHP_EOL);
     }
 }
